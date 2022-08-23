b105d325c92c0a28be8867819df197e1ba06ef7a
Coi <thithibuibui1@gmail.com>
2022-08-24 00:34:34 +0700

    push ham tim min max trong mot mang

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void min_max()
+        {
+            Console.Write("Nhap n phan tu: ");
+            string n = Console.ReadLine();
+            int a = Convert.ToInt32(n);
+            int[] arr = new int[a];
+            for (int i = 0; i < a; i++)
+            {
+                string nhap = Console.ReadLine();
+                int temp = Convert.ToInt32(nhap);
+                arr[i] = temp;
+            }
+            int min = arr[0], max = arr[0];
+            for (int i = 1; i < a; i++)
+            {
+                if (max < arr[i]) max = arr[i];
+                if (min > arr[i]) min = arr[i];
+            }
+            Console.WriteLine("Max = "+max);
+            Console.WriteLine("Min = "+min);
+            Console.ReadLine();
+        }
+        static void Main(string[] args){
+            min_max();
+        }
+    }
+}
